b2824995effd325201cad59fae14cad57ac2bbaf
Stephane AMAOUA <stephane.amaoua@live.com>
2015-09-04 17:40:03 +0100

    First commit. product_caterory_sequence is nearly done. The other one is empty

---
+++ product_category_sequence/product_category_sequence.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+from openerp.tools.translate import _
+
+import logging
+_logger = logging.getLogger(__name__)
+
+########################################################
+## First Add the sequence_id field to product.category #
+########################################################
+class ProductCategory(models.Model):
+    _inherit = 'product.category'
+    x_sequence_id = fields.Many2one(comodel_name='ir.sequence',
+                                    string='Product Sequence',
+                                    help="This sequence will be use for products' internal references")
+
+##################################################
+# Now each time we create a product
+#   - Get its category sequence (upward)
+#   - Set sequence found or default one
+##################################################
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    default_code = fields.Char(
+        string='Reference',
+        size=64,
+        select=True,
+        required=False,
+        default='Auto')
+
+    _sql_constraints = [
+        ('uniq_default_code',
+         'unique(default_code)',
+         'The reference must be unique'),
+    ]
+
+    def _getInternalReference(self, vals):
+        # Looking for category sequence bottom-->up
+        categ_id = vals['categ_id']
+        sequence_id  = sequence_id = self.env['product.category'].browse(categ_id).x_sequence_id.id
+        while sequence_id == False:
+            categ_id = self.env['product.category'].browse(categ_id).parent_id.id
+            if categ_id == False:
+                break
+            sequence_id  = sequence_id = self.env['product.category'].browse(categ_id).x_sequence_id.id
+
+        if sequence_id is False:
+            _logger.info('Category has no sequence set ! Setting default sequence')
+            ref_code = self.env['ir.sequence'].search([('name', '=', 'Default Product Sequence')])._next()
+        else:
+            _logger.info('Product category sequence found !')
+            ref_code = self.env['ir.sequence'].get_id(sequence_id)
+            
+        _logger.info('ref_code: %s', ref_code)
+        return ref_code
+    
+    @api.model
+    def create(self, vals):
+        if 'default_code' in vals and vals['default_code'] == 'Auto':
+            vals['default_code'] = self._getInternalReference(vals)
+        return super (ProductTemplate, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        for product in self:
+            if 'default_code' in vals and vals['default_code'] == 'Auto':
+                vals['default_code'] = self._getInternalReference(vals)
+            super(ProductTemplate, product).write(vals)
+        return True
---
+++ quality_control_stock_samples/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class quality_control_stock_samples(models.Model):
+#     _name = 'quality_control_stock_samples.quality_control_stock_samples'
+
+#     name = fields.Char()
