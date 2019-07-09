75da751377db0e6630fc48b9c8b9f84dcff6a3c8
Stéphane AMAOUA <stephane.amaoua@live.com>
2019-07-09 17:10:10 +0100

    Module ICE v8

---
+++ ice_lb/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2010 kazacube (http://kazacube.com).
+
+{
+    'name': 'Maroc ICE',
+    'author': 'AgenceZ7',
+    'category': 'Localization',
+    'description': """""",
+    'depends': ['base', 'account'],
+    'installable': True,
+    'data': [
+        'views/res_company_views.xml',
+        'views/res_partner_views.xml',
+        #'views/report_footer.xml',
+        #'views/report_invoice.xml',
+    ],
+}
