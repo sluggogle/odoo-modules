b45875a59219245fff158c67cab0a5de1df2df99
Stéphane AMAOUA <stephane.amaoua@live.com>
2019-07-11 20:20:23 +0100

    [New] az7_reports Report for sale   - No saleperson   - No time in the quotation date   - Add ICE for moroccan companies

---
+++ az7_reports/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'AZ7 Reports',
+    'version': '12.0.1.0.0',
+    'author': 'AgenceZ7',
+    'category': 'Localization',
+    'description': """
+        AgenceZ7 Reports
+    """,
+    'depends': ['sale_management', 'ln10_ma_ice'],
+    'data': [
+        'views/sale_report_templates.xml',
+    ],
+    'installable': True,
+}
