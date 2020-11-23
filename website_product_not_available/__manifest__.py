bc200be748f589fbc7249ac7ffc327be67fb3229
Steff <amaouas@gmail.com>
2020-11-23 19:28:49 +0100

    [website_product_not_available] working initial commit

---
+++ website_product_not_available/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright Monweblocal 2020
+{
+    'name': 'Website Product Not Available',
+    'version': '14.0.1.0.0',
+    'summary': 'Option to show a product in the shop while it cannot be added to cart.',
+    'description': """
+        This module adds a new option in the product availability for products not available but still showed in the shop.
+        User may define a custom message to customers.
+    """,
+    'author': 'Monweblocal',
+    'website': 'https://monweblocal.fr',
+    'category': 'Website/Website',
+    'depends': [
+        'website_sale_stock',
+    ],
+    'data': [
+        'views/product_template_views.xml',
+        'views/website_sale_stock_templates.xml',
+    ],
+    'demo': [
+        ''
+    ],
+    'auto_install': False,
+    'application': False,
+}
