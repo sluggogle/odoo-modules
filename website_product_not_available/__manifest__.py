07dba721bb15a04a5a05a42cbbf195617763106b
Sluggogle <amaouas@gmail.com>
2021-02-10 19:40:49 +0100

    [website_product_not_available] IMP: Hide price when 0

--- website_product_not_available/__manifest__.py
+++ website_product_not_available/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright Monweblocal 2020
 {
     'name': 'Website Product Not Available',
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.1.0',
     'summary': 'Option to show a product in the shop while it cannot be added to cart.',
     'description': """
         This module adds a new option in the product availability for products not available but still showed in the shop.
-        User may define a custom message to customers.
+        User may define a custom message to customers. Set item price to 0 to hide the price.
     """,
     'author': 'Monweblocal',
     'website': 'https://monweblocal.fr',
