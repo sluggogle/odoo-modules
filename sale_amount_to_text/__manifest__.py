5f54c4a613644fae85aa3398df47ec9bb985e7fc
Sluggogle <amaouas@gmail.com>
2021-03-09 18:04:24 +0100

    [sale_mount_to_text] This module adds a computed field with the amount int words in both sale.order and account.move

---
+++ sale_amount_to_text/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Amount to text',
+    "summary": 'A field with the amount in text',
+    'version': '14.0.1.0.0',
+    'author': 'Stephane AMAOUA',
+    'category': 'Sales',
+    'description': """
+        Adds a computed field in sale.order and account.invoice to display the amount by words in reports.
+    """,
+    'depends': [
+        'sale_management',
+    ],
+    'data': [
+
+    ],
+    'application': False,
+    'installable': True,
+}
