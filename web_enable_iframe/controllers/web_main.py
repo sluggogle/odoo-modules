6d3fbf33cf7de4f723ab87dd5e537d94510bdcb1
Sluggogle <amaouas@gmail.com>
2021-07-16 10:02:49 +0100

    [FIX] web_enable_iframe: Back to iframe ok but no session

--- web_enable_iframe/controllers/web_main.py
+++ web_enable_iframe/controllers/web_main.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright Monweblocal 2021
 
+import logging
 from odoo.addons.web.controllers.main import Home
 
 from odoo import http, _
 from odoo.http import request
+
+import logging
+_logger = logging.getLogger(__name__)
 
 class CustomHome(Home):
 
@@ -12,12 +16,11 @@ class CustomHome(Home):
     def web_client(self, s_action=None, **kw):
         res = super(CustomHome, self).web_client(s_action, **kw)
         res.headers.pop('X-Frame-Options', None)
-        res.headers['Set-Cookie'] = res.headers['Set-Cookie'] + 'SameSite=None'
+        _logger.info(res.headers['Set-Cookie'])
         return res
 
     @http.route('/web/login', type='http', auth="none", csrf=False)
     def web_login(self, redirect=None, **kw):
         res = super(CustomHome, self).web_login(redirect, **kw)
         res.headers.pop('X-Frame-Options', None)
-        res.headers['Set-Cookie'] = res.headers['Set-Cookie'] + 'SameSite=None'
         return res
