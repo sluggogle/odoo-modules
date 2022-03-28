6c0d46641f21fa4e602d3a2eddef6415b1845e47
Sluggogle <amaouas@gmail.com>
2022-03-28 10:06:31 +0000

    [ADD] sale_manual_confirmation_date

---
+++ sale_manual_confirmation_date/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Manual Confirmation Date',
+    "summary": 'Modify confirmation date afterwards',
+    'version': '14.0.1.0.0',
+    'author': 'Stephane AMAOUA',
+    'category': 'Sales',
+    'description': """
+        User is able to modify the sale order confirmation date afterwards.
+    """,
+    'depends': [
+        'sale_management',
+    ],
+    'data': [
+    ],
+    'application': False,
+    'installable': True,
+}
